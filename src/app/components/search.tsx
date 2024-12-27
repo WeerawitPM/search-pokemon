9d3c160267179b305db87b0b5da0db2083b493af
WeerawitPM <63030079@kmitl.ac.th>
2024-12-27 21:43:20 +0700

    update: new search box design

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -15,13 +15,26 @@ const Search = () => {
 
     return (
         <div>
-            <input
-                type="text"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder="Search Pokémon"
-            />
-            <button onClick={handleSearch}>Search</button>
+            <label className="input input-bordered flex items-center gap-2">
+                <input
+                    type="text"
+                    placeholder="Search for a Pokémon"
+                    className="w-full grow"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+                />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="h-4 w-4 opacity-70">
+                    <path
+                        fillRule="evenodd"
+                        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                        clipRule="evenodd" />
+                </svg>
+            </label>
         </div>
     );
 };
